edb76963b59215cc85861dbc2bbdd89dccc815db
Ricardo Ramos Z <42349266+ramosric@users.noreply.github.com>
2018-10-16 12:14:45 -0400

    Add files via upload

---
+++ introduccion.py
@@ -0,0 +1,16 @@
+# Este es un comentario en Python (# al principio)
+
+"""
+Esta es otra forma de comentar en Python
+es muy útil para múltiples líneas de
+comentarios (uso de triple comilla doble, al
+principio y fin del comentario
+"""
+
+# A nivel de Cónsola en python
+print("Hola amigos amantes del Zorro")
+
+"""
+En Foxpro y VisualFoxpro el código en la ventana de comandos
+? "Hola amigos amantes del Zorro"
+"""
